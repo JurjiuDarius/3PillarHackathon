46c3fd594fa8f594dd7da9f1854ad9308927ea7e
TAVI <octavianpatilea@gmail.com>
2024-04-19 15:24:30 +0300

    Updated file list

--- frontend/src/app/home/file-list/file-list.component.ts
+++ frontend/src/app/home/file-list/file-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {UploadComponent} from "./upload/upload.component";
 import {MatListModule} from "@angular/material/list";
 import {NgForOf} from "@angular/common";
 import {EBook} from "../../models/ebook";
 import {FileListService} from "./file-list.service";
 import {Subscription} from "rxjs";
+import {UploadService} from "./upload/upload.service";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @Component({
@@ -13,21 +15,33 @@ import {Subscription} from "rxjs";
   imports: [
     UploadComponent,
     MatListModule,
-    NgForOf
+    NgForOf,
+    MatButtonModule
   ],
   templateUrl: './file-list.component.html',
   styleUrl: './file-list.component.scss'
 })
-export class FileListComponent {
+export class FileListComponent implements OnDestroy {
   files: EBook[] = []
-  fileSubscription: Subscription;
+  fileUploadSubscription: Subscription;
 
-  constructor(private service: FileListService) {
-    this.fileSubscription = this.service.getDocumentsForUser().subscribe(
-      (files: EBook[]) =>
-      {
-        this.files = files;
+  constructor(private service: FileListService, private uploadService: UploadService) {
+    this.fileUploadSubscription = this.uploadService.uploadComplete$.subscribe(
+      () => {
+        this.service.getDocumentsForUser().subscribe(
+          (files: EBook[]) =>
+          {
+            console.log('Files:', files);
+            this.files = files;
+          }
+        );
       }
     );
+
+    this.uploadService.notifyUploadComplete()
+  }
+
+  ngOnDestroy() {
+    this.fileUploadSubscription.unsubscribe();
   }
 }
